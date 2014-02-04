d589452cc35e366697ce964d1ebe8a5446c89d6b
katsutoshi.ochi <ochi0218@gmail.com>
2014-02-04 20:43:08 +0900

    task c done.

--- depot/test/controllers/products_controller_test.rb
+++ depot/test/controllers/products_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
+  fixtures :products
+
   setup do
     @product = products(:one)
     @update = {
@@ -15,6 +17,8 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    
+    assert_select '#data tr', 3
   end
 
   test "should get new" do
