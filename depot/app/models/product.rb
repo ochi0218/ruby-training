a398a2f887d7eebdb823081f2199dcc54797a227
katsutoshi.ochi <ochi0218@gmail.com>
2014-02-04 21:27:18 +0900

    task d done.

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,6 +1,20 @@
 class Product < ActiveRecord::Base
+    has_many :line_items
+    before_destroy :ensure_not_referenced_by_any_line_item
+
     validates :title, :description, :image_url, presence: true
     validates :price, numericality: { greater_than_or_equal_to: 0.01 }
     validates :title, uniqueness: true
     validates :image_url, allow_blank: true, format: { with: /^*\.(gif|jpg|png)$/i, message: 'is permit jpg or png or gif.', multiline: true }
+
+    private
+
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+          return true
+      else 
+          errors.add(:base, '品目が存在します')
+          return false
+      end
+    end
 end
